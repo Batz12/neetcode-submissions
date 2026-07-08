a331e243e7b7350b3d2ade53a7bca5df9c5e6e68
Aneesh Acharya <75311568+Batz12@users.noreply.github.com>
2026-07-08 22:35:06 +0530

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        int size = 0;
+
+        for(int num : nums) {
+            int left = 0;
+            int right = size;
+
+            while(left < right) {
+                int mid = left + (right - left) / 2;
+
+                if(dp[mid] < num) { // we are storing in dp the smallest ending values
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            dp[left] = num;
+
+            if(left == size) {
+                size++;
+            }
+        } 
+
+        return size;
+    }
+}
