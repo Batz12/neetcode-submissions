8f294dd8306e432106151b0b5f1c9dfbf34f3ac0
Aneesh Acharya <75311568+Batz12@users.noreply.github.com>
2026-07-16 00:52:27 +0530

    Add: regular-expression-matching - submission-2

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[] next = new boolean[n + 1];
+
+        next[n] = true;
+
+        for(int j = n - 2; j >= 0; j--) {
+            if(p.charAt(j + 1) == '*') {
+                next[j] = next[j + 2];
+            }
+        }
+
+        for(int i = m - 1; i >= 0; i--) {
+            boolean[] curr = new boolean[n + 1];
+
+
+            for(int j = n - 1; j >= 0; j--) {
+                boolean firstMatch = (i < m) && ((s.charAt(i) == p.charAt(j)) || (p.charAt(j) == '.'));
+
+                if((j + 1 < n) && p.charAt(j + 1) == '*') {
+                    curr[j] = curr[j + 2] || (firstMatch && next[j]);
+                } else {
+                    curr[j] = firstMatch && next[j + 1];
+                }
+                
+            }
+
+            next = curr;
+        }
+
+        return next[0];
+    }
+}
